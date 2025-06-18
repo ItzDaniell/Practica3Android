9a526832bc6765c1e41f466b40648cd798507d97
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-06-17 19:11:32 -0500

    Main View Model

--- app/src/main/java/com/rodriguez/juan/laboratoriocalificado03/TeacherApi.kt
+++ app/src/main/java/com/rodriguez/juan/laboratoriocalificado03/TeacherApi.kt
@@ -3,8 +3,6 @@ import retrofit2.Response;
 import retrofit2.http.GET;
 
 interface TeacherApi {
-    public interface TeacherApi {
-        @GET("/list/teacher")
-        suspend fun getTeachers(): Response<TeacherListResponse>
-    }
+    @GET("/list/teacher")
+    suspend fun getTeachers(): Response<TeacherListResponse>
 }
